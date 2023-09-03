98840bd620f69cfc5f1876966e316be1ecd07d35
otaviootavio <otaviovacari@usp.br>
2023-09-03 10:53:05 -0300

    remove dotenv ( uses docker )

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import { Pool } from "pg";
-import dotenv from "dotenv";
 import express, { NextFunction, Request, Response, response } from "express";
 import { getUsers } from "./queries";
 
 const app = express();
-dotenv.config(); //Reads .env file and makes it accessible via process.env
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.send("hi");
