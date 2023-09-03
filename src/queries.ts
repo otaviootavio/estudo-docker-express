75aa0f49a3a12e1f2c54cbcc63b70cea97aee9d4
otaviootavio <otaviovacari@usp.br>
2023-09-03 15:33:43 -0300

    implement add query

--- src/queries.ts
+++ src/queries.ts
@@ -1,5 +1,6 @@
 import { Response } from "express";
 import { Pool } from "pg";
+import { Employee } from "./types";
 
 const pool:Pool = new Pool({
     host: process.env.DB_HOST,
@@ -13,6 +14,13 @@ const pool:Pool = new Pool({
     const results = await pool.query('SELECT * FROM public.employees ORDER BY id ASC');
     response.json( results.rows );
   }
+
+  const addEmployee = async (employee: Employee): Promise<Employee> => {
+    const query = 'INSERT INTO public.employees (name, position, age) VALUES ($1, $2, $3) RETURNING *';
+    const values = [employee.name, employee.position, employee.age];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  }
    
 
-export {getUsers}
+export {getUsers, addEmployee}
