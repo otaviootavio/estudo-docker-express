02d55e4510baee23adfb643d0a8482d0ee0b46d9
otaviootavio <otaviovacari@usp.br>
2023-09-03 15:34:14 -0300

    new add employee route

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,32 @@
 import { Pool } from "pg";
 import express, { NextFunction, Request, Response, response } from "express";
-import { getUsers } from "./queries";
+import { getUsers, addEmployee } from "./queries";
+import { Employee } from "./types";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.send("hi");
 });
 
-app.get("/users", (req: Request, res: Response, next: NextFunction) => {
+app.get("/employee", (req: Request, res: Response, next: NextFunction) => {
   getUsers(res).catch(next);
 });
+
+app.post("/employee", async (req: Request, res: Response, next: NextFunction) => {
+  const employeeData: Employee = req.body;
+  
+  console.log(req.body)
+
+  if(!employeeData.name || !employeeData.age || !employeeData.position){
+    return res.status(400).send('Bad Requet');
+  }
+  
+  const newEmployee = await addEmployee(employeeData);
+  res.status(201).json(newEmployee);
+})
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
